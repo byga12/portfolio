4498b8b00e7792c7ccf2c26601a367e9a8aea876
Gabriel Shimabuku <gaby.shima12@gmail.com>
2022-03-10 07:59:25 -0300

    Set unstable_revalidate path to /projects

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    await res.unstable_revalidate("/");
+    await res.unstable_revalidate("/projects");
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
