49eb42cf7fe34f0636108ac5c7f22b076c31d209
Gabriel Shimabuku <gaby.shima12@gmail.com>
2022-03-26 22:47:50 -0300

    Removed base64Image feature. Images are now obtained from Dropbox raw URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["s3.us-west-2.amazonaws.com"],
+    domains: [
+      "s3.us-west-2.amazonaws.com",
+      "dl.dropbox.com",
+      "dl.dropboxusercontent.com",
+    ],
   },
 };
 
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -3,7 +3,6 @@ import s from "./Projects.module.sass";
 import useTransitionOnScroll from "../../hooks/useTransitionOnScroll";
 import { useRef } from "react";
 import Project from "../../components/Project/Project";
-import axios from "axios";
 
 export async function getStaticProps() {
   const projects = await getProjectsList({
@@ -12,28 +11,6 @@ export async function getStaticProps() {
       equals: "Live",
     },
   });
-
-  for (let i = 0; i < projects.length; i++) {
-    const desktopImage = await axios.get(
-      projects[i].properties["Desktop image"].files[0].file.url,
-      { responseType: "arraybuffer" }
-    );
-    const mobileImage = await axios.get(
-      projects[i].properties["Mobile image"].files[0].file.url,
-      { responseType: "arraybuffer" }
-    );
-    const rawDesktop = Buffer.from(desktopImage.data).toString("base64");
-    const rawMobile = Buffer.from(desktopImage.data).toString("base64");
-    const desktopBase64Image =
-      "data:" + desktopImage.headers["content-type"] + ";base64," + rawDesktop;
-    const mobileBase64Image =
-      "data:" + desktopImage.headers["content-type"] + ";base64," + rawMobile;
-
-    projects[i].properties["Desktop image"].files[0].file.url =
-      desktopBase64Image;
-    projects[i].properties["Mobile image"].files[0].file.url =
-      mobileBase64Image;
-  }
 
   return { props: { projects } };
 }
@@ -54,8 +31,8 @@ const Projects = (props) => {
             key={id}
             name={properties["Name"].title[0]?.plain_text}
             description={properties["Description"].rich_text[0]?.plain_text}
-            desktopImg={properties["Desktop image"].files[0]?.file.url}
-            mobileImg={properties["Mobile image"].files[0]?.file.url}
+            desktopImg={properties["Desktop image"]?.url}
+            mobileImg={properties["Mobile image"]?.url}
             mainTechnologies={properties["Main technologies"]?.multi_select}
             repo={properties["Repo URL"].url}
             live={properties["Live URL"].url}
