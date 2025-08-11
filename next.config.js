9733f56b5f8bfac37ec598b72d988709c107744c
Gaby Shima <byga>
2025-08-11 02:06:47 -0300

    Allow all domains

--- next.config.js
+++ next.config.js
@@ -2,26 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "s3.us-west-2.amazonaws.com",
-      "dl.dropbox.com",
-      "dl.dropboxusercontent.com",
-      "dropbox.com",
-      "pcloud.com",
-      "*.pcloud.com",
-      "th1.pcloud.com",
-      "th2.pcloud.com",
-    ],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.dl.dropboxusercontent.com',
+        hostname: '*',
       },
-      {
-        protocol: 'https',
-        hostname: '*.pcloud.com',
-      },
-    ]
+    ],
+    unoptimized: true
   },
 };
 
