e6ddf5b7fddf94bb249409f836eecfc52d0cb7d7
Gaby Shima <byga>
2025-08-11 02:13:55 -0300

    test 3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true
+    domains: [
+      "s3.us-west-2.amazonaws.com",
+      "dl.dropbox.com",
+      "dl.dropboxusercontent.com",
+      "dropbox.com",
+      "th1.pcloud.com",
+      "th2.pcloud.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.dl.dropboxusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.pcloud.com',
+      },
+    ]
   },
 };
 
