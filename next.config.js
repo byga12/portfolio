aeacbd5aea58feec98a53db1a90aae20791f7302
Gaby Shima <byga>
2025-08-11 02:23:19 -0300

    Remove domains. All images are stored locally as assets

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "s3.us-west-2.amazonaws.com",
-      "dl.dropbox.com",
-      "dl.dropboxusercontent.com",
-      "dropbox.com",
-      "th1.pcloud.com",
-      "th2.pcloud.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.dl.dropboxusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.pcloud.com',
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
